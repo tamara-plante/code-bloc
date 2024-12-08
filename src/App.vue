2b1cafd77a428934cf763a74ea165b5b2b3af075
Tamara Plante <0631064@johnabbottcollege.net>
2024-12-07 21:21:47 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <input
+  type="text"
+  placeholder="Type here"
+  class="input input-bordered input-error w-full max-w-xs" />
+</template>
+
+<style scoped>
+</style>
